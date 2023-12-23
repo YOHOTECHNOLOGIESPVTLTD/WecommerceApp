814ddce1345834123883f78b7aa329dd1005a889
Mohammed Thasthakir <mohammed.mt392@gmail.com>
2023-12-23 16:54:17 +0530

    thasthakir commit

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+import theme, {
+  COLORS,
+  SIZES,
+  FONTS,
+  darkTheme,
+  lightTheme,
+  selectedTheme,
+} from "./theme";
+import icons from "./icons";
+import constansts from "./constansts";
+
+export {
+  theme,
+  COLORS,
+  SIZES,
+  FONTS,
+  darkTheme,
+  lightTheme,
+  selectedTheme,
+  icons,
+  constansts
+};
